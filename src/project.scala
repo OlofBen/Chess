db27888841f05eb3337eef078f40ab7b6cedac45
Olof Bengtsson <ol1058be-s@student.lu.se>
2023-11-06 09:15:04 +0100

    Added dep

--- src/project.scala
+++ src/project.scala
@@ -2,4 +2,5 @@
 //> using option -unchecked -deprecation
 //> using option -Wunused:all -Wvalue-discard -Ysafe-init
 //> using dep org.scalatest::scalatest::3.2.17
+//> using dep io.github.tors42:chariot:0.0.76
 
